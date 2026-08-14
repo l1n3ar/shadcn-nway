4fe4adcebc4cf96599bac0e907a30d2848b58dd1
Mehul Chattopadhyay <m@Mehuls-MacBook-Pro.local>
2026-08-14 13:55:46 +0530

    Vercel Analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
+import { Analytics } from "@vercel/analytics/next"
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
     >
       <body>
+      <Analytics />
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
